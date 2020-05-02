88b7a8fe2510cbc99042a21bf224993d43200f54
Troopy <40128411+troopaloop8@users.noreply.github.com>
2020-05-02 11:06:52 +0900

    mvp done, working on stretch

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import axiosWithAuth from "../utils/axiosWithAuth"
 
 const initialColor = {
   color: "",
@@ -10,6 +10,7 @@ const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const [newColor, setNewColor] = useState(initialColor);
 
   const editColor = color => {
     setEditing(true);
@@ -21,10 +22,43 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+    .put(`/colors/${colorToEdit.id}`, colorToEdit)
+    .then(res => {
+      updateColors(
+        colors.map(color => {
+          return color.id === res.data.id ? res.data : color;
+        })
+      )
+    })
+    .catch(err => console.log(err))
   };
+
+  
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+    .delete(`/colors/${color.id}`, colorToEdit)
+    .then(res => {
+      updateColors(
+        colors.filter(color => {
+          return color.id !== res.data
+        })
+      )
+    })
+    .catch(err => console.log(err))
+  };
+
+  const addColor = e => {
+    e.preventDefault()
+    axiosWithAuth()
+    .post(`/colors`, newColor)
+    .then(res => {
+      updateColors([...colors, newColor]);
+      setNewColor(initialColor);
+    })
+    .catch(err => console.log(err))
   };
 
   return (
@@ -80,8 +114,32 @@ const ColorList = ({ colors, updateColors }) => {
           </div>
         </form>
       )}
+      <form onSubmit={addColor}>
+          <p>add color</p>
+          <label>
+            color name:
+            <input 
+            placeholder="color name"
+            value={newColor.color}
+            onChange={e => {
+              setNewColor({...newColor, color: e.target.value})
+            }}
+            />
+          </label>
+          <label>
+            hex code:
+            <input 
+            placeholder="#ffffff"
+            value={newColor.code.hex}
+            onChange={e => {
+              setNewColor({ ...newColor, code: { hex: e.target.value } })
+            }}
+            />
+          </label>
+          <button type="submit">Add Color</button>
+        </form>
       <div className="spacer" />
-      {/* stretch - build another form here to add a color */}
+        
     </div>
   );
 };
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const login = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/login", user)
+      .then((res) => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/bubbles");
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div>
+      <div>
+        <h1>Welcome to React Bubbles!</h1>
+        <h2>Let's Get Poppin!</h2>
+      </div>
+      <div>
+        <form onSubmit={login}>
+          <div>
+            <input
+              name="username"
+              placeholder="username"
+              onChange={handleChange}
+              value={user.username}
+            />
+          </div>
+          <div>
+            <input
+              name="password"
+              placeholder="password"
+              onChange={handleChange}
+              value={user.password}
+            />
+          </div>
+          <div>
+            <button>Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
